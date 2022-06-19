45d5a57c1b997e52f2c2b0e7d9627469e84a2614
xiaoguo <812177025@qq.com>
2022-06-19 21:17:41 +0800

    测试GitHub在线运行项目

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+/**
+ * @Description
+ * @Author xiaoguo
+ * @CreateTime 2022-06-19 21:14
+ * @Version
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello GitHub!");
+    }
+}
